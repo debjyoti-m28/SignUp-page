5519744aa7ecd73fb8e16375c7b4139c03413196
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-07-18 03:18:15 +0530

    user validation

--- client/src/components/FormInput.jsx
+++ client/src/components/FormInput.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./formInput.css"
 
 export const FormInput = (props) => {
+    const [focused, setFocuesd] = useState(false);
     const {id, onChange,errorMsg, ...inputProps} = props;
+    
+    const handleFocus = (e) =>{
+        setFocuesd(true);
+    }
+
   return (
     <div className='formInput'> 
-        <input {...inputProps} onChange={onChange}/>
+        <input {...inputProps} 
+        onChange={onChange} 
+        onBlur={handleFocus} 
+        focused={focused.toString()}
+        onFocus={()=>inputProps.name==="cpassword" && setFocuesd(true)}
+        />
         <span>{errorMsg}</span>
 
     </div>
